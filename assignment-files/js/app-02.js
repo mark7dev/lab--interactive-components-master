91fe8ca6860ba574f1a4390ab8d8c8dc9e87919e
mark7dev <mcamacho_ce7@live.com.mx>
2018-07-30 09:39:13 -0500

    version02

--- assignment-files/js/app-02.js
+++ assignment-files/js/app-02.js
@@ -1,79 +1,99 @@
 /* 02 - FORM VALIDATION :: YOUR CODE BELOW */
 
 
-//USERNAME
-var username = formulario.elements.username;
-var usernameText = username.value;
+// General Variables
+document.querySelector('.validation--username').textContent = "";
+var formText = document.forms.generalForm;
 
-//PASSWORD
-var password = formulario.elements.userpassword;
-var passwordText =  password.value;
+//Function
 
-//CONFIRM PASSWORD
-var confirmPass = formulario.elements.confirmpassword;
-var confirmPassText = confirmPass.value;
+formText.addEventListener('submit', function(e) {
+  e.preventDefault();
 
-//TAX ID
-var taxId = formulario.elements.taxid;
-var taxIdText = taxId.value;
+// Local Variables
 
-//ACCOUNT TYPE
-var account = formulario.elements.account;
-var accountSelect = account.value;
+  var userInput = e.target.elements.username.value;
+  var passwordInput = e.target.elements.userpassword.value;
+  var passwordInputConf = e.target.elements.confirmpassword.value;
+  var taxIdNum = e.target.elements.taxid.value;
+  var listOfAccounts = formText.elements.account.value;
+  var checkBoxTrmsOfSvc = formText.elements.termsofservice;
 
-//TERMS OF SERVICE
-var terms =  formulario.elements.termsofservice;
-var termsCheck = terms.checked;
+// Output Variables
 
-/*START SECTION TO COMPROBATE VALUES*/
+  var userValidText = document.querySelector('.validation--username');
+  var passwordValidText = document.querySelector('.validation--user-password');
+  var confirmPasswordValidText = document.querySelector('.validation--confirm-password');
+  var taxIdValidText = document.querySelector('.validation--tax-id');
+  var accountValidText = document.querySelector('.validation--account');
+  var termsOfServiceValidText = document.querySelector('.validation--terms-of-service');
 
-//USERNAME
-if(usernameText === "") {
-    document.querySelector('.validation--username').textContent = 'This field can not be blank'
-} else if(usernameText !== "") {
-    document.querySelector('.validation--username').textContent = 'Success'}
+  var validationOfEntireForm = document.querySelector('.validation--entire-form');
 
-//PASSWORD
-if(passwordText === "" || passwordText.length < 8) {
-    document.querySelector('.validation--user-password').textContent = 'The password must have a minimum of 8 carecteres'
-} else if(passwordText.length > 8) {
-    document.querySelector('.validation--user-password').textContent = 'Success';}
+// Validation of username
 
-//CONFIRM PASSWORD
-if(confirmPassText !== passwordText || passwordText === "") {
-    document.querySelector('.validation--confirm-password').textContent = 'Password must match';
-} else if(confirmPassText === passwordText) {
-    document.querySelector('.validation--confirm-password').textContent = 'Success';
-}
 
-//TAX ID
-if(typeof taxId !== "number" && taxId.length < 10) {
-    document.querySelector('.validation--tax-id').textContent = 'Tax ID is only a numbers and is 10 digits';
-} else if(typeof taxId === "number" && taxId.length === 10) {
-    document.querySelector('.validation--tax-id').textContent = 'Success';
-}
 
-//ACCOUNT TYPE
-if(accountSelect === "") {
-    document.querySelector('.validation--account').textContent = "Must select account type";
-} else if (accountSelect !== "") {
-    document.querySelector('.validation--account').textContent = "Success";
-}
+  if (userInput === "") {
+    userValidText.textContent = "Username cannot be blanck";
+  } else {
+    userValidText.textContent = "Success";
+  }
 
-//TERMS OF SERVICE
-if(termsCheck === false) {
-    document.querySelector('.validation--terms-of-service').textContent = 'Must approve terms of service';
-} else if(termsCheck === true) {
-    document.querySelector('.validation--terms-of-service').textContent = 'Success';
-}
+// Validation of Password
 
-//FOMR CHECKOUT
-if(usernameText !== "" && passwordText.length > 8 && confirmPassText === passwordText && typeof taxId === "number" && taxId.length === 10 && accountSelect !== "" && termsCheck === true) {
-    document.querySelector('.validation--entire-form').textContent = 'Form Complete'
-    var form = document.forms.formulario;
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-    })  
-} else if(usernameText === "" || passwordText === "" || passwordText.length < 8 || typeof taxId !== "number" && taxId.length < 10 || accountSelect === "" || termsCheck === false) {
 
-}
+  if (passwordInput.length < 8) {
+    passwordValidText.textContent = "Passwords must have a minimum of 8 characters";
+  } else {
+    passwordValidText.textContent = "Success";
+  }
+
+// Validation of Confirm Password
+
+
+  if (passwordInputConf !== passwordInput || passwordInputConf === "") {
+    confirmPasswordValidText.textContent = "Passwords must match";
+  } else {
+    confirmPasswordValidText.textContent = "Success";
+  }
+
+// Validation of Tax id
+
+  if (taxIdNum === "") {
+    taxIdValidText.textContent = "Must provide Tax ID Number ";;
+  } else if (taxIdNum*1 > 0 && taxIdNum.length === 10 ) {
+    taxIdValidText.textContent = "Success";
+  } else {
+    taxIdValidText.textContent = "Tax ID Number is only numbers and is 10 digits";
+  }
+
+// Validation of Account list options
+
+  if (listOfAccounts === "") {
+    accountValidText.textContent = "Must select account type";
+  } else {
+    accountValidText.textContent = "Success";
+  }
+
+// Validation of Check Box
+
+    if(checkBoxTrmsOfSvc.checked === false) {
+    termsOfServiceValidText.textContent = "Must approve terms of service";
+  } else {
+    termsOfServiceValidText.textContent = "Success";
+  }
+
+// Validation of FORM
+
+  if ((userValidText.textContent,
+  passwordValidText.textContent,
+  confirmPasswordValidText.textContent,
+  taxIdValidText.textContent,
+  accountValidText.textContent,
+  termsOfServiceValidText.textContent) === 'Success') {
+    validationOfEntireForm.textContent = "Form Completed";
+  }
+
+
+})
